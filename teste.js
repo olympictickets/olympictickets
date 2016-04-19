82b372a6945f0300526653b50e2eda7e094bbecb
olympictickets <olympictickets16@gmail.com>
2016-04-19 20:35:49 -0300

    Added files via upload

--- teste.js
+++ teste.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name       Olympic Tickets
 // @namespace  https://ingressos.rio2016.com
-// @version    0.18
+// @version    1
 // @description  Ajuda a comprar ingressos
 // @match      https://ingressos.rio2016.com/rio2016.html*
-// @author     Diogo Maron
+// @author     Olympic Tickets
 // @grant GM_xmlhttpRequest
-
 // ==/UserScript==
 
+var segundos = 2;
+
+$(function() {
+	alert(segundos);
+});
